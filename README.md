d69345e30a095b442f3be31c5d575f00d3e8d4d5
Ian Marshall <ian.js.tutor@gmail.com>
2022-09-20 18:27:55 -0400

    adding basic files

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# css-buttons
+# CSS Buttons
+
+[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+
+## Description
+
+CSS animations are awesome and can do some pretty amazing things. Here are some interesting CSS buttons direct from my imagination.
+
+## Author
+
+> [Ian Marshall](https://ianjstutor.github.io/ian-marshall/)
+
+## Live Site
+
+> [https://ianjstutor.github.io/css-buttons/](https://ianjstutor.github.io/css-buttons/)
+
+## Usage
+
+Find the button set you like, copy its CSS code, and give an element the matching class name. Done!
---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+const rulesList = document.styleSheets[2];
+const cssRules = [...rulesList.cssRules];
+
+document.querySelector("header button").addEventListener("click", ({currentTarget: target}) => {
+    if (target.textContent.startsWith("Pause")) {
+        //pause all animations
+        target.innerHTML = "Play All <span>&#9658;</span>";
+        document.querySelectorAll(".spinner").forEach(s => s.classList.add("pause"));
+    }
+    else {
+        //play all animations
+        target.innerHTML = "Pause All <span>&#10074;&#10074;</span>";
+        document.querySelectorAll(".spinner").forEach(s => s.classList.remove("pause"));
+    }
+});
+
+document.querySelectorAll("main a").forEach(a => {
+    a.addEventListener("click", e => {
+        e.preventDefault();
+        const paused = a.classList.contains("pause");
+        if (paused) a.classList.remove("pause");
+        const className = a.className.replace("spinner ", "");
+        const rules = cssRules.filter(r => r.selectorText?.includes(className) || r.name?.includes(className)).map(r => r.cssText);
+        const aside = document.createElement("aside");
+        aside.innerHTML = `<div>
+            <button>Close <span>&times;</span></button>
+            <span class="spinner ${className}"></span>
+            <p>${rules.join("</p><p>")}</p>
+        </div>`;
+        document.body.append(aside);
+        document.querySelector("aside:last-of-type button").addEventListener("click", e => {
+            aside.outerHTML = "";
+            if (paused) a.classList.add("pause");
+        });
+    });
+});
